023fb373dc9a5933d40558c4cc7b88b09a8d03b6
Rahul Mohan <mohanrahul93@gmail.com>
2023-06-27 21:24:51 +0530

    array code js

---
+++ 27june/scripts/index.js
@@ -0,0 +1,12 @@
+const arr = [1,2,3];
+// console.log(arr.pop());
+console.log(arr.shift());
+console.log(arr);
+arr.unshift(1);
+arr.unshift(0);
+arr.unshift(-1);
+console.log(arr);
+
+console.log(arr.length);
+
+console.dir(arr);
