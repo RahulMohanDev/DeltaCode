25f775cef2bb77864c6a4ac5077d964667285537
Rahul Mohan <mohanrahul93@gmail.com>
2023-07-14 22:03:45 +0530

    add code commit before break

---
+++ 14July-main/index.js
@@ -0,0 +1,86 @@
+// spread and rest (...)
+
+// const person = {
+//   name: "rahul",
+// };
+// const address = {
+//   doorNo: 548,
+//   area: "indranagar",
+// };
+
+// const data = { ...person, ...address };
+
+// console.log(data);
+
+// const test = {
+//     age: 23
+// }
+
+// const testCopy = {...test};
+
+// testCopy.age = 55;
+
+// console.log(test)
+
+const test1 = {
+  name: "rahul mohan",
+};
+
+const test2 = {
+  name: "Eshita",
+};
+
+const test3 = {
+  name: "Alok",
+  age: 28,
+};
+
+const merge = { ...test1, ...test2, ...test3 };
+
+// console.log(merge);
+
+// const person = {
+//   name: "Rahul Mohan",
+//   address: {
+//     area: "test",
+//   },
+// };
+
+// const person2 = { ...person,
+//     address: { ...person.address },
+//     name: "Naveen"
+// };
+
+// const person2 {
+//     name: "naveen"
+//     address: 1000
+// }
+
+// person.address.area = "potato";
+
+// console.log(person);
+// console.log(person2);
+
+// assignment
+
+// const complex = {
+//   person: {
+//     name: "Rahul Mohan",
+//     age: 27,
+//     address: {
+//       area: { name: "indranagar" },
+//       houseNo: 546,
+//     },
+//   },
+//   vehicle: {
+//     type: "cycle",
+//     brand: {
+//       name: "bitwin",
+//     },
+//   },
+// };
+
+// const arr = [1, 2, 3];
+// const arr2 = [3, 4, 5];
+// const newArray = [...arr, ...arr2, 6, 7];
+// console.log(newArray);
---
+++ 14july-rev/assets/index.js
@@ -0,0 +1,87 @@
+/* primitives */
+
+// if the value is
+// Falsy values
+// null
+// undefined
+// false
+// NaN
+// 0
+// -0
+// ""
+
+// if(undefined){
+//     console.log("hi")
+// }
+
+// if(-0){
+//     console.log("hi")
+// }
+
+// if("   "){
+//     console.log("hi")
+// }
+
+// if ({}){
+//   console.log("what about this");
+// }
+
+// let a=null;
+// if(a){console.log("hi")}
+
+// const a = 22;
+// let b = 'test';
+
+// const obj = {
+//   name: "rahul Mohan",
+//   age: 27,
+//   key: "test",
+// };
+
+// // bracket notation and dot notation
+// console.log(obj.age);
+
+// const key = "name";
+// obj[key];
+// // rahul mohan
+// obj.key
+// // test
+// // console.log(obj[1]);
+// // not cool !
+
+// const friends = [
+//     "Rohan singh poona",
+//     "Ankur",
+//     "Siddarth"
+// ]
+
+// friends[0];
+// // "Rohan singh poona",
+
+// friends.length;
+
+// const person = {
+//   name: "Rahul Mohan",
+//   age: 27,
+//   address: {
+//     area: "banglore",
+//     doorNo: 547,
+//   },
+// };
+
+// let areaCopy = person.address.area;
+// areaCopy = "Delhi";
+// console.log(person.address.area)
+
+// const person = {
+//   name: "Rahul Mohan",
+//   age: 27,
+//   address: {
+//     area: {name: "banglore"},
+//     doorNo: 547,
+//   },
+// };
+
+// let areaCopy = person.address.area;
+// areaCopy.name = "Delhi";
+// console.log(person.address.area.name);
